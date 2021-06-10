0f67607c9af7407ea1ee588327773860fa64b022
mgstoyanov <84728686+mgstoyanov@users.noreply.github.com>
2021-06-10 13:47:38 -0500

    Create mustache.rb

---
+++ mustache.rb
@@ -0,0 +1,23 @@
+class mustache < Formula
+  desc "This library is an implementation of the Mustache template language in Go."
+  homepage "https://github.com/cbroglie/mustache"
+  url "https://github.com/cbroglie/mustache/archive/refs/tags/v1.2.2.tar.gz"
+  sha256 "97d929608b2f5c6b1318225f50dc3b773f3d645f74b670cbe9c3e95940e025ff"
+
+  depends_on "go"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "on"
+    ENV["GOFLAGS"] = "-mod=vendor"
+    ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
+    (buildpath/"src/github.com/cbroglie/mustache").install buildpath.children
+    cd "src/github.com/cbroglie/mustache" do
+      system "go", "build", "-o", bin/"mustache", "."
+    end
+  end
+
+  test do
+    assert_match /mustache data.yml template.mustache/, shell_output("#{bin}/mustche -h", 0)
+  end
+end
