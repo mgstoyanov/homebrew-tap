a90b8c2c42f7a6a785c5ba0fc28a1648c06a4637
Martin Stoyanov <martin.stoyanov@vertalo.com>
2021-12-21 12:44:13 -0600

    adding js mustache

--- mustache.rb
+++ mustache.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Mustache < Formula
   desc "Mustache templating in javascript"
   homepage "https://github.com/janl/mustache.js"
-  url "https://registry.npmjs.org/mustacje/-/mustache-4.2.0.tgz"
+  url "https://registry.npmjs.org/mustache/-/mustache-4.2.0.tgz"
   sha256 "c8b1d437305bc4c492cb9b85bb1b91edb5cc74fa91bd007616746c6258dae6c5"
 
   depends_on "node"
